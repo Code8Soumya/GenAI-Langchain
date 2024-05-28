9003a755b4c325de3fe71bf1064e23db934a433d
Code8Soumya <ssenapati721@gmail.com>
2024-05-28 13:02:32 +0530

    started creating advanced chatbot

--- simple-chatbot/app.py
+++ simple-chatbot/app.py
@@ -6,12 +6,15 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+# openai api key
 os.environ["OPENAI_API_KEY"]=os.getenv("openai_api_key")
-## Langmith tracking
+
+# langsmith tracking
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("langchain_project_simple_chatbot")
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("langchain_api_key")
 
-## Prompt Template
+# prompt template
 prompt=ChatPromptTemplate.from_messages(
     [
         ("system","You are a helpful assistant. Please response to the user queries"),
@@ -19,11 +22,11 @@ prompt=ChatPromptTemplate.from_messages(
     ]
 )
 
-## streamlit framework
-st.title("A simple chatbot with gpt-4o")
+# streamlit framework
+st.title("A simple chatbot with gpt-3.5-turbo")
 input_text=st.text_input("Start conversation")
 
-# openAI LLm 
+# openai llm
 llm=ChatOpenAI(model="gpt-3.5-turbo-0125")
 output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
